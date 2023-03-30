4eeb6711fd38d6202fd2bf02ffb5fc46f0a711b8
Humbertico04 <humbertico2004@gmail.com>
2023-03-30 09:37:35 +0200

    det recursivo

--- Pregunta_6.py
+++ Pregunta_6.py
@@ -16,3 +16,24 @@ class Matriz():
 
     def __str__(self):
         return str(self.matriz)
+
+    def submatriz(self, fila, columna):
+        sub = Matriz(self.filas - 1, self.columnas - 1)
+        for i in range(self.filas):
+            for j in range(self.columnas):
+                if i != fila and j != columna:
+                    sub_fila = i if i < fila else i - 1
+                    sub_columna = j if j < columna else j - 1
+                    sub.asignar_elemento(sub_fila, sub_columna, self.obtener_elemento(i, j))
+        return sub
+    
+def determinante_recursivo(matriz):
+    if matriz.filas == 1 and matriz.columnas == 1:
+        return matriz.obtener_elemento(0, 0)
+    elif matriz.filas == 2 and matriz.columnas == 2:
+        return matriz.obtener_elemento(0, 0) * matriz.obtener_elemento(1, 1) - matriz.obtener_elemento(0, 1) * matriz.obtener_elemento(1, 0)
+    else:
+        det = 0
+        for j in range(matriz.columnas):
+            det += ((-1) ** j) * matriz.obtener_elemento(0, j) * determinante_recursivo(matriz.submatriz(0, j))
+        return det
