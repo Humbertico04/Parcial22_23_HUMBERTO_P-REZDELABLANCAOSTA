304338fb219f96be64f7d890ef09fcc6a675b1e5
Humbertico04 <humbertico2004@gmail.com>
2023-03-30 08:55:31 +0200

    mergesort

--- Pregunta_2.py
+++ Pregunta_2.py
@@ -19,4 +19,38 @@ def excepciones(lista):
         elif i % 10 == 0 and i < 200:
             print(i)
 
-excepciones(lista)
+def mergesort(lista):
+    """Método de ordenamiento mergesort"""
+    if (len(lista) <= 1):
+        return lista
+    else:
+        medio = len(lista)//2
+        izquierda = []
+        for i in range(0, medio):
+            izquierda.append(lista[i])
+        derecha = []
+        for i in range(medio, len(lista)):
+            derecha.append(lista[i])
+        izquierda = mergesort(izquierda)
+        derecha = mergesort(derecha)
+        if (izquierda[-1] <= derecha[0]):
+            izquierda += derecha
+            return izquierda
+        resultado = merge(izquierda, derecha)
+        return resultado
+    
+def merge(izquierda, derecha):
+    """Función para mezclar listas"""
+    lista_mezclada = []
+    while (len(izquierda) > 0) and (len(derecha) > 0):
+        if (izquierda[0] < derecha[0]):
+            lista_mezclada.append(izquierda.pop(0))
+        else:
+            lista_mezclada.append(derecha.pop(0))
+    if (len(izquierda) > 0):
+        lista_mezclada += izquierda
+    if (len(derecha) > 0):
+        lista_mezclada += derecha
+    return lista_mezclada
+
+lista = mergesort(lista)
