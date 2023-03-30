5bd7df26cc23a66b395665d9f9f292cfd8bc330a
Humbertico04 <humbertico2004@gmail.com>
2023-03-30 09:00:34 +0200

    2 compl

--- Pregunta_2.py
+++ Pregunta_2.py
@@ -54,3 +54,33 @@ def merge(izquierda, derecha):
     return lista_mezclada
 
 lista = mergesort(lista)
+
+def binaria(lista, buscado):
+    """Método de búsqueda binaria"""
+    posicion = -1
+    primero = 0
+    ultimo = len(lista)-1
+    while (primero <= ultimo) and (posicion == -1):
+        medio = (primero + ultimo) // 2
+        if (lista[medio] == buscado):
+            posicion = medio
+        else:
+            if (buscado < lista[medio]):
+                ultimo = medio - 1
+            else:
+                primero = medio + 1
+    return posicion
+
+print(f"El número buscado se encuentra en la posición {binaria(lista, 145)} según python, para un humano está en la posición {binaria(lista, 145)+1}")
+
+# PRUEBAS
+import unittest
+
+class Test(unittest.TestCase):
+    def test_mergesort(self):
+        self.assertEqual(mergesort([18, 50, 210, 80, 145, 333, 70, 30]), [18, 30, 50, 70, 80, 145, 210, 333])
+    def test_binaria(self):
+        self.assertEqual(binaria([18, 30, 50, 70, 80, 145, 210, 333], 145), 5)
+
+if __name__ == "__main__":
+    unittest.main()
