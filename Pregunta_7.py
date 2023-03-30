350a86dbddd190f83efeb2569e05c7cd53f5da6b
Humbertico04 <humbertico2004@gmail.com>
2023-03-30 08:39:54 +0200

    existe termino

--- Pregunta_7.py
+++ Pregunta_7.py
@@ -136,4 +136,12 @@ class Polinomio(object):
                     aux.sig = aux.sig.sig
                     to_remove.sig = None
 
-    
+    def existe_termino(polinomio, termino):
+        """Verifica si existe un termino en el polinomio"""
+        aux = polinomio.termino_mayor
+        while (aux is not None and aux.info.termino != termino):
+            aux = aux.sig
+        if (aux is not None):
+            return True
+        else:
+            return False
